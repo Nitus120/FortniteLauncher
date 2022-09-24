4a6ce0d7ac7ae967904ac9eead93d384b1747bf3
noteason <74929228+noteason@users.noreply.github.com>
2022-09-24 00:57:24 -0400

    Update MainWindow.xaml.cs

--- FortniteLauncher/MainWindow.xaml.cs
+++ FortniteLauncher/MainWindow.xaml.cs
@@ -107,7 +107,7 @@ namespace FortniteLauncher
 
                         string FN = System.IO.Path.Combine(folder, "FortniteGame\\Binaries\\Win64\\FortniteClient-Win64-Shipping.exe");
 
-                        string arguments = "-AUTH_LOGIN=unused -AUTH_PASSWORD=" + exchange + " -AUTH_TYPE=exchangecode -epicapp=Fortnite -epicenv=Prod -epiclocale=en-us -epicportal -nobe -fromfl=eac -fltoken=3db3ba5dcbd2e16703f3978d -caldera=eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiYmU5ZGE1YzJmYmVhNDQwN2IyZjQwZWJhYWQ4NTlhZDQiLCJnZW5lcmF0ZWQiOjE2Mzg3MTcyNzgsImNhbGRlcmFHdWlkIjoiMzgxMGI4NjMtMmE2NS00NDU3LTliNTgtNGRhYjNiNDgyYTg2IiwiYWNQcm92aWRlciI6IkVhc3lBbnRpQ2hlYXQiLCJub3RlcyI6IiIsImZhbGxiYWNrIjpmYWxzZX0.VAWQB67RTxhiWOxx7DBjnzDnXyyEnX7OljJm-j2d88G_WgwQ9wrE6lwMEHZHjBd1ISJdUO1UVUqkfLdU5nofBQ - skippatchcheck";
+                        string arguments = "-PutYourOwnArgs";
 
                         Process Fortnite = new Process
                         {
